9ec2b62388cb578ef0a8b92bebb82143a834a640
odair.tiodaia95@gmail.com <odair.tiodaia95@gmail.com>
2021-04-20 18:00:56 -0300

    Add project files.

---
+++ CalculadoraPOO.consoleapp/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPOO.consoleapp
+{
+
+    //Requisitos//
+    /*A Dona Mariana precisa de um sistema para calcular operações matemáticas simples, como adição, subtração, multiplicação e divisão.Ela gostaria também de visualizar um histórico das operações realizadas.
+     * Requisito 01
+        Nossa calculadora deve ter a possibilidade de somar dois números
+
+        Requisito 02
+        Nossa calculadora deve ter a possibilidade fazer várias operações de soma
+
+        Requisito 03
+        Nossa calculadora deve ter a possibilidade fazer várias operações de soma e de subtração
+
+        Requisito 04
+        Nossa calculadora deve ter a possibilidade fazer as quatro operações básicas da matemática
+
+        Requisito 05
+        Nossa calculadora deve validar a opções do menu
+
+        BUG 01
+        Nossa calculadora deve realizar as operações com "0"
+
+        Requisito 06
+        Nossa calculadora deve permitir visualizar as operações já realizadas
+        Critérios:
+
+        1 - A descrição da operação realizada deve aparecer assim, exemplo:
+           
+         2 + 2 = 410 - 5 = 5    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            operacoes calculadora = new operacoes();
+            while (true)
+            {
+                calculadora.Menu();
+
+                calculadora.opcao = Console.ReadLine();
+
+                if (calculadora.OpcaodeValidacao(calculadora.opcao))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Opção escolhida é  inválida Tente Novamente");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                if (calculadora.OpcaodeVisualizacao(calculadora.opcao))
+                {
+                    if (calculadora.OperacoesRealizadas == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("Nenhuma operação foi criada até o momento.");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        for (int i = 0; i < calculadora.operacoesRealizadas.Length; i++)
+                        {
+                            if (calculadora.operacoesRealizadas[i] != null)
+                                Console.WriteLine(calculadora.operacoesRealizadas[i]);
+                        }
+                    }
+                    Console.ReadLine();
+                    Console.Clear();
+
+                    continue;
+                }
+
+                if (calculadora.EhOpcaoSair(calculadora.opcao))
+                {
+                    break;
+                }
+
+                Console.Write("Digite o primeiro número: ");
+                calculadora.primeiroNumero = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite o segundo número: ");
+                calculadora.segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+                calculadora.Operacoes(calculadora.opcao, ref calculadora.resultado, calculadora.primeiroNumero, calculadora.segundoNumero, ref calculadora.simboloOperacao);
+
+                string operacaoRealizada =
+                    calculadora.primeiroNumero.ToString() + " " + calculadora.simboloOperacao + " " +
+                    calculadora.segundoNumero.ToString() + " = " + calculadora.resultado.ToString();
+
+                calculadora.operacoesRealizadas[calculadora.OperacoesRealizadas] = operacaoRealizada;
+
+                calculadora.OperacoesRealizadas++;
+
+                Console.WriteLine();
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+        }
+
+    }
+}
---
+++ CalculadoraPOO.consoleapp/operacoes.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPOO.consoleapp
+{
+    class operacoes
+    {
+        public string[] operacoesRealizadas = new string[50];
+        public string opcao = "";
+        public double resultado = 0;
+        public int OperacoesRealizadas = 0;
+        public string simboloOperacao = "";
+        public double primeiroNumero, segundoNumero;
+
+        public bool OpcaodeVisualizacao(string opcao)
+        {
+            return opcao == "5";
+        }
+
+        public void Operacoes(string opcao, ref double resultado, double primeiroNumero, double segundoNumero, ref string simboloOperacao)
+        {
+
+            switch (opcao)
+            {
+                case "1":
+
+                    resultado = primeiroNumero + segundoNumero;
+                    simboloOperacao = "+";
+                    Console.WriteLine("A soma dos numeros Totalizaram: " + resultado); break;
+                case "2":
+
+                    resultado = primeiroNumero - segundoNumero;
+                    simboloOperacao = "-";
+                    Console.WriteLine("a subtração totalizou: " + resultado); break;
+                case "3":
+
+                    resultado = primeiroNumero * segundoNumero;
+                    simboloOperacao = "*";
+                    Console.WriteLine("o Resultado da sua Multiplicação foi: " + resultado); break;
+                case "4":
+
+                    if (segundoNumero == 0)
+                    {
+                        Console.WriteLine("Atenção na Operação de Divisão do segundo numero não pode ser igual ha 0");
+                        break;
+                    }
+                    resultado = primeiroNumero / segundoNumero;
+                    simboloOperacao = "/";
+                    Console.WriteLine("O resultado da sua divisão foi : " + resultado); break;
+
+            }
+        }
+
+        public bool EhOpcaoSair(string opcao)
+        {
+            return opcao.Equals("s", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public bool OpcaodeValidacao(string opcao)
+        {
+            return opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" && opcao != "5" && opcao != "S" && opcao != "s";
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("Calculadora da Dona Maria em POO");
+            Console.WriteLine("");
+            Console.WriteLine("Escolha através do menu a Operação que Deseja Efetuar: ");
+            Console.WriteLine("Digite 1 para Fazer uma Soma '+'. ");
+            Console.WriteLine("Digite 2 para Fazer uma Subtração '-'. ");
+            Console.WriteLine("Digite 3 para Fazer uma Multiplicação '*'. ");
+            Console.WriteLine("Digite 4 para Fazer uma Divisão '/'. ");
+            Console.WriteLine("Digite 5 para visualizar as operações realizadas até o momento. ");
+            Console.WriteLine("Digite S para sair do App.");
+        }
+
+    }
+
+}
